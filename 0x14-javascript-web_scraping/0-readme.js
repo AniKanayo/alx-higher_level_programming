bc94357286823cee221e5f4a590e7569c5335a19
AniKanayo <micanian4real@gmail.com>
2023-04-25 22:52:17 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-
-
-import sys
-
-# Get the file path from the first argument
-file_path = sys.argv[1]
-
-# Try to open the file and read it
-try:
-    with open(file_path, "r", encoding="utf-8") as f:
-        content = f.read()
-        print(content)
-except Exception as e:
-    print(e)
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	  console.log(error || content);
+});
