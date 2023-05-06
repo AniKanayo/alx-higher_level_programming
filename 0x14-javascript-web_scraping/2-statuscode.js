6667b56417e3875412f43a2e9b30a898348976c2
AniKanayo <micanian4real@gmail.com>
2023-05-06 01:13:17 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-//A script that computes the number of tasks completed by user id
 const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
